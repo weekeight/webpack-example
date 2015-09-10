4ae9819559ed60c14598d0faac68515d70f7aa6a
陈锋 <chenf@shinemo.com>
2015-09-10 11:21:07 +0800

    增加支持iconfont构建配置

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,14 @@ module.exports = {
 		            'file?hash=sha512&digest=hex&name=[hash].[ext]',
 		            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
 		        ]
+		    },
+		    {
+		        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+		        loader: "url-loader?limit=10000&minetype=application/font-woff"
+		    },
+		    {
+		    	test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?/,
+		        loader: 'file-loader'
 		    }
 		]
 	},
